f4c168e12d2d77d7c2e5d24b90932309b7a2dfc5
rejas <github@veeck.de>
2021-11-28 23:05:09 +0100

    Update key compare function

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ process.stdin.setRawMode(true);
 let audio = new Audic();
 audio.volume = config.volume;
 
+const compareKeys = (key1, key2) => {
+    if (key1.name && key2.name)
+        return (key1.name === key2.name && key1.meta === key2.meta);
+    else if (key1.sequence && key2.sequence)
+        return (key1.sequence === key2.sequence && key1.meta === key2.meta);
+
+    return false;
+};
+
 // Start the keypress listener for the process
 process.stdin.on('keypress', async (str, key) => {
 
@@ -34,7 +43,7 @@ process.stdin.on('keypress', async (str, key) => {
     // User has triggered a keypress, now do whatever we want!
     let soundFile;
     for (let entry of mappings) {
-        if (entry.key.name === key.name && entry.key.meta === key.meta) {
+        if (compareKeys(entry.key, key)) {
             soundFile = entry.file;
         }
     }
