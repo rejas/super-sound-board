872b5aa483e023f2885513ff1d33e20d7496f6fc
veeck <michael@veeck.de>
2022-09-05 23:26:40 +0200

    Monday night updates

--- config.js
+++ config.js
@@ -20,10 +20,34 @@ module.exports = {
             "key": {"name": "y", "meta": false}, "file": "cat.mp3"
         },
         {
+            "key": {"name": "2", "meta": false}, "file": "phone.mp3"
+        },
+        {
+            "key": {"name": "4", "meta": false}, "file": "thunder.mp3"
+        },
+        {
+            "key": {"name": "6", "meta": false}, "file": "clock.mp3"
+        },
+        {
+            "key": {"name": "7", "meta": false}, "file": "ball.mp3"
+        },
+        {
+            "key": {"name": "8", "meta": false}, "file": "drop.mp3"
+        },
+        {
             "key": {"name": "f3", "meta": false}, "file": "drum.mp3"
         },
         {
+            "key": {"name": "f9", "meta": false}, "file": "piano.mp3"
+        },
+        {
+            "key": {"name": "f11", "meta": false}, "file": "police.mp3"
+        },
+        {
             "key": {"name": "f12", "meta": false}, "file": "door.mp3"
+        },
+        {
+            "key": {"sequence": "*", "meta": false}, "file": "guitar.mp3"
         },
         {
             "key": {"sequence": ".", "meta": false}, "file": "train.mp3"
